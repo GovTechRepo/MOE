13f1c0c2cee541f27cdad2c5443eca569bd10dd9
IDACTMO002 <IDACTMO002@GOVTECH-CTMO-02>
2017-01-13 09:42:37 +0800

    Signed-off-by: IDACTMO002 <IDACTMO002@GOVTECH-CTMO-02>

---
+++ src/test/java/com/moe/Logoutt.java
@@ -0,0 +1,13 @@
+package com.moe;
+
+import org.testng.annotations.Test;
+
+public class Logoutt {
+	
+	@Test
+	private void pubic() {
+		// TODO Auto-generated method stub
+             System.out.println("Successfully logout");
+	}
+
+}
